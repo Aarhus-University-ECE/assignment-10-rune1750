4592fecfa4fc6607d1353641dd4c1d528621aca7
rune1750 <rune.rune@live.dk>
2022-11-15 08:33:42 +0100

    Formatted version

--- src/btree.c
+++ src/btree.c
@@ -6,94 +6,107 @@
 #include <stdlib.h>   
 
 // Function to create a new node
-struct tree_node *create_node(int x)
+struct tree_node* create_node(int x)
 {
   struct tree_node* branch = (struct tree_node*)malloc(sizeof(struct tree_node));
   branch->data = x;
-  branch->left = NULL; 
-  branch->right = NULL; 
+  branch->left = NULL;
+  branch->right = NULL;
 
   return branch;
 }
 
 // Function to insert data into tree
-struct tree_node *Insert(int x, struct tree_node *t) {
-  if(t==NULL){
+struct tree_node* Insert(int x, struct tree_node* t) {
+  if (t == NULL) {
     // if the tree is empty, a new node is made as the root. 
-    t = create_node(x);   
-  } else if(x <= t->data){
+    t = create_node(x);
+  }
+  else if (x <= t->data) {
     // The place to store the data in the tree is checked recursively
-    t->left = Insert(x,t->left); 
-  } else {
-    t->right = Insert(x,t->right);
+    t->left = Insert(x, t->left);
+  }
+  else {
+    t->right = Insert(x, t->right);
   }
   return t;
 }
 
-struct tree_node *Remove(int x, struct tree_node *t) {
-  if(t==NULL){
+struct tree_node* Remove(int x, struct tree_node* t) {
+  if (t == NULL) {
     return t;
     // Looking for input x  
-  } else if(x < t->data){
-  t->left = Remove(x,t->left);
-  } else if(x > t->data){
+  }
+  else if (x < t->data) {
+    t->left = Remove(x, t->left);
+  }
+  else if (x > t->data) {
     t->right = Remove(x, t->right);
-  } else 
-  // If the input is not greater nor lesser than the data of the left/right
-  // There are a two different options to consider
+  }
+  else
+    // If the input is not greater nor lesser than the data of the left/right
+    // There are a two different options to consider
   {
     // Temporary node for storing the tree
-    struct node *temp = t;
+    struct node* temp = t;
     // 1: In case there is no right/left child
-    if(t->left == NULL && t->right == NULL){
+    if (t->left == NULL && t->right == NULL) {
       free(t);
-      t = NULL; 
-    // 2: There are children
-    } else if(t->right == NULL){
-    t = t->left;
-    free(temp); 
-    } else if(t->left == NULL){
-    t = t->right;
-    free(temp);  
+      t = NULL;
+      // 2: There are children
+    }
+    else if (t->right == NULL) {
+      t = t->left;
+      free(temp);
+    }
+    else if (t->left == NULL) {
+      t = t->right;
+      free(temp);
       // 2.1 there are two children
-      }else{
-        if(t->left->data > t->right->data){
+    }
+    else {
+      if (t->left->data > t->right->data) {
         t = t->right;
         free(temp);
-          } else{
-            t = t->left;
-            free(temp);
-          }
-}
-}
-return t;
+      }
+      else {
+        t = t->left;
+        free(temp);
+      }
+    }
+  }
+  return t;
 }
 
-int Contains(int x, struct tree_node *t) {
-  if(t == NULL){
-    return false; 
+int Contains(int x, struct tree_node* t) {
+  if (t == NULL) {
+    return false;
     // recursive search for the input
-  } else if(t->data == x){
-    return true; 
-  } else if(x <= t->data){
+  }
+  else if (t->data == x) {
+    return true;
+  }
+  else if (x <= t->data) {
     return Contains(x, t->left);
-  } else{
+  }
+  else {
     return Contains(x, t->right);
   }
 }
 
-struct tree_node *Initialize(struct tree_node *t) {
-t = NULL; 
+struct tree_node* Initialize(struct tree_node* t) {
+  t = NULL;
 
-return t; 
+  return t;
 }
 
 // Function to check if tree is empty
-int Empty(struct tree_node *t) {
-  if(t==NULL){
-  return true; 
-  } else {
-    return false; 
+int Empty(struct tree_node* t) {
+  if (t == NULL) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
@@ -102,4 +115,4 @@ int Empty(struct tree_node *t) {
 int Full(struct tree_node *t) {
   // Test if full
   return 0;
-} */ 
+} */
--- src/linked_list.c
+++ src/linked_list.c
@@ -5,16 +5,16 @@
 #include <math.h>
 
 /* functions to create lists */
-node *make_node(int v, node *q) {
-  node *p = malloc(sizeof(node));
+node* make_node(int v, node* q) {
+  node* p = malloc(sizeof(node));
   p->value = v;
   p->next = q;
   return p;
 }
 
 /* free all nodes in the list p */
-void free_list(node *p) {
-  node *q;
+void free_list(node* p) {
+  node* q;
   while (p != NULL) {
     q = p->next;
     free(p);
@@ -24,23 +24,23 @@ void free_list(node *p) {
 
 // Prints data to console and runs the void again 
 // with the node *p updated to be the next element in the array
-void print_list(node *p) {
-if(p != NULL){
-printf("%d \n", p->value);  
-print_list(p->next);  
-}
+void print_list(node* p) {
+  if (p != NULL) {
+    printf("%d \n", p->value);
+    print_list(p->next);
+  }
 }
 
 // Uses <math.h> pow function to calculate the square of numbers
 // the sum of the numbers is returned
-int sum_squares(node *p) {
+int sum_squares(node* p) {
   // Add your code for excercise 2
   // You can find the tests in tests.cpp
-  if(p==NULL){
-    return 0; 
+  if (p == NULL) {
+    return 0;
   }
-  else{
-  return square(p->value) + sum_squares(p->next);
+  else {
+    return square(p->value) + sum_squares(p->next);
   }
 }
 
@@ -48,13 +48,14 @@ typedef int (*fn_int_to_int)(int);
 
 // Function to take a linked list and create a new linked list
 // With integer values squared from the previous linked list
-node *map(node *p, fn_int_to_int f) { 
-  if (p == NULL){
-    return NULL; 
-  }else{
-    return make_node(f(p->value), map(p->next, f)); 
+node* map(node* p, fn_int_to_int f) {
+  if (p == NULL) {
+    return NULL;
   }
-  return NULL; 
+  else {
+    return make_node(f(p->value), map(p->next, f));
+  }
+  return NULL;
 }
 
 int square(int x) { return x * x; }
